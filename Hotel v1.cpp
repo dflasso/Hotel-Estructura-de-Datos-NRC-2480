b01ae0668624e9288672102eb0a421822d5e9f70
DiegoBorja7 <daborja1@espe.edu.ec>
2017-11-22 16:43:07 -0500

    Creacion de funcion Ingreso

--- Hotel v1.cpp
+++ Hotel v1.cpp
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
 #include "libHote.h"
-//estructuras
+#include "libValidation.h"
+
+/*Declaracion de estructura*/
 typedef struct SCuarto{ ///inf. cuartos
 	bool disponible;
 	float precio;
@@ -7,36 +14,89 @@ typedef struct SCuarto{ ///inf. cuartos
 	char descripcion[20];
 	char CI[11];
 }Cuarto;
+
 typedef struct SPersona{ ///contiene los datos personales
 	char nombre[20];
 	char apellido[20];
 	char celular[11];
 	char CI[11];
+	char email [50];
 	int edad;
 	Cuarto cuarto;
 }Persona;
+
 typedef struct SCliente{ ///Lista de clientes
 	Persona cliente;
 	struct SCliente *siguiente;
 	struct SCliente *anterior;
 }*Cliente;
+
 typedef struct SHabitacion{ //habitaciones del hotel
 	Cuarto cuarto1;
 	Cuarto cuarto2;
 	Cuarto cuarto3;
 }Habitacion;
+
 typedef struct SDia{ //lista que simula un calendario de los dias del mes
 	Habitacion habitacion;
 	struct SDia *siguiente;
 	struct SDia *anterior;
 }*Dia;
+
 typedef struct SMes{ //lista de meses del año
 	Dia dia;
 	struct SMes *siguiente;
 	struct SMes *anterior;
 }*Mes;
-//declaracion
-//desarrollo
-int main(){
+
+/*Declaracion de Funciones*/
+void ingresoDeCliente();
+
+/*Desarollo de Funciones*/
+void ingresoDeCliente()
+{
+	SPersona datos;
+	do
+	{
+		printf("Ingrese el nombre: ");
+		gets(datos.nombre);
+		fflush(stdin);
+	}while(validacionNombreYApellido(datos.nombre));
+	do
+	{
+		printf("Ingrese el apellido: ");
+		gets(datos.apellido);
+		fflush(stdin);
+	}while(validacionNombreYApellido(datos.apellido));
+	do
+	{
+		printf("Ingrese su numero de cedula: ");
+		gets(datos.CI);
+		fflush(stdin);
+	}while(validacionCedula(datos.CI));
+	do
+	{
+		printf("Ingrese su email: ");
+		gets(datos.email);
+		fflush(stdin);
+	}while(validacionEmail(datos.email));
+	do
+	{
+		printf("Ingrese su celular: ");
+		fflush(stdin);
+		gets(datos.celular);
+	}while(validacionCelular(datos.celular));
+	
+	do
+	{
+		printf("Ingrese su edad: ");
+		scanf("%d",&datos.edad);
+	}while(validacionEdad(datos.edad));
+	
+}
+
+int main()
+{
+	ingresoDeCliente();
 	return 0;
 }
--- libValidation.h
+++ libValidation.h
@@ -3,14 +3,11 @@
 #include <conio.h>
 #include <string.h>
 
-bool validacionCedula(char Cedula[],char *msg){
+bool validacionCedula(char Cedula[]){ 
     long int ced,coc,var1;
-	char cedula[15];
     int numpar,numimpar,v,digito,mul,res2,sumat,A[15],aux;
     numpar=numimpar=0;
-    printf("%s",msg);
-	scanf("%s",&cedula);
-	ced=atol(cedula);
+	ced=atol(Cedula);
 	var1=ced;
 	var1=(var1/100000000);
 	if ((var1>=1)&&(var1<=24))
@@ -45,13 +42,12 @@ bool validacionCedula(char Cedula[],char *msg){
 	            v=0;
 	    if(v==digito)
 	    {
-	        //printf("\nCedula valida\n");
 	        aux=1;
-	        strcpy(Cedula,cedula);
+	        //strcpy(Cedula,cedula);
 	        return false;
 	    }
 	    else{
-	    	printf("_____________________\n\nCedula Incorrecta !!\n_____________________\n");
+	    	printf("_____________________\n\nCedula Incorrecta !!\n_____________________\n\n");
 	        aux=-1;
 	        return true;
 		}
@@ -59,45 +55,42 @@ bool validacionCedula(char Cedula[],char *msg){
 	}
 	else
 	{
-	    printf("_____________________\n\nCedula Incorrecta !!\n_____________________\n");
+	    printf("_____________________\n\nCedula Incorrecta !!\n_____________________\n\n");
 	    aux=-1;
 	    return true;
 	}
 	if(aux==1)
 	{
-		strcpy(Cedula,cedula);
+		//strcpy(Cedula,cedula);
 		return false;
 	}
 	else
 	{
-		strcpy(Cedula,"0");
+		//strcpy(Cedula,"0");
 	}
 }
-bool validacionNombreYApellido(char cadena[],char *msg){
+
+bool validacionNombreYApellido(char cadena[]){
 	int i;
-	printf("%s",msg);
-	gets(cadena);
-	for(i=0;cadena[i]!=NULL;i++)
+	for(i=0;cadena[i]!='\0';i++)
 	{
 		if((cadena[i]<65||cadena[i]>90)&&(cadena[i]<97||cadena[i]>122)&&cadena[i]!=32)
 		{
-			printf("_____________________________________________________\nDato Incorrecto !!\nNo puede contener caracteres especiales, ni numeros\nVuelva a ingresar por favor\n_____________________________________________________\n");
+			printf("______________________________________________________\nDato Incorrecto !!\nNo puede contener caracteres especiales, ni numeros\nVuelva a ingresar por favor\n______________________________________________________\n\n");
 			return true;
 			break;
 		}	
-	}
+	}	
 	return false;
 }
-bool validacionCelular(char celular[],char *msg){
+
+bool validacionCelular(char celular[]){
 	int i;
-	printf("%s",msg);
-	gets(celular);
-	
 	for(i=0;celular[i]!=NULL;i++)
 	{
 		if(celular[i]<48||celular[i]>57)
 		{
-			printf("_________________________________________________________________________\nNumero de celular incorrecto !!\nVerifique su numero de celular...\nLos dos primeros digitos deben ser 09 y debe tener 10 digitos en total\n_________________________________________________________________________\n");
+			printf("_________________________________________________________________________\nNumero de celular incorrecto !!\nVerifique su numero de celular...\nLos dos primeros digitos deben ser 09 y debe tener 10 digitos en total\n_________________________________________________________________________\n\n");
 			return true;
 			break;
 		}
@@ -107,26 +100,24 @@ bool validacionCelular(char celular[],char *msg){
 		return false;
 	}
 }
-bool validacionNumero(char numero[],char *msg){
+
+bool validacionNumero(char numero[]){
 	int i;
-	printf("%s",msg);
-	gets(numero);
 	for(i=0;numero[i]!=NULL;i++)
 	{
 		if(numero[i]<48||numero[i]>57)
 		{
-			printf("___________________________________________________\nDato Incorrecto !!\nNo puede contener caracteres especiales, ni letras\nVuelva a ingresar por favor\n___________________________________________________\n");
+			printf("___________________________________________________\nDato Incorrecto !!\nNo puede contener caracteres especiales, ni letras\nVuelva a ingresar por favor\n___________________________________________________\n\n");
 			return true;
 			break;
 		}
 	}
 	return false;	
 }
-bool validacionEmail(char email[],char *msg){
+
+bool validacionEmail(char email[]){
 	int i;
 	bool arroba=true,com;
-	printf("%s",msg);
-	gets(email);
 	for(i=0;email[i]!=NULL;i++)
 	{
 		if(email[i]==64)
@@ -143,4 +134,15 @@ bool validacionEmail(char email[],char *msg){
 	
 	return arroba||com;
 }
+
+bool validacionEdad(int edad)
+{
+	if(edad<66&&edad<18)
+	{
+			printf("______________________________________________________________________\nEdad no permitida, tiene que ser mayor a 18 y menor a 66 a%cos de edad.\nVuelva a ingresar por favor\n______________________________________________________________________\n\n",164);
+			return true;
+	}
+	return false;
+}
+
 
