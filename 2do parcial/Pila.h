dc2e3868ca7d2c5cb5f6245da76c4884d8f10f18
dflasso <dannylasso0909@hotmail.com>
2018-01-18 22:32:08 -0500

    backup v1.0

--- 2do parcial/Pila.h
+++ 2do parcial/Pila.h
@@ -14,9 +14,14 @@ class Pila{
 		Nodo* getPalabras();
 		void generarPila();
 		void push(char []);
-		void pop(char []);
+		void pushNew(char [],char []);
+		void pushNew(char [],char[],int );
+		void popPalabra(char []);
 		void buscar(char []);
+		void buscar(char palabra[],Pila *&palabrasBuscadas);
 		void imprimir();
+		void nombreBakcup(char[],int );
+		void crearBackup(char[],int );
 };
 
 void Pila::setPalabras(Nodo* palabras_)
@@ -71,7 +76,8 @@ void Pila::imprimir()
 	{
 		printf(" =>\t%s\t\t\t%s\n",nuevo->getEspaniol(),nuevo->getIngles());
 		nuevo=nuevo->getSiguienteDireccion();
-	}	
+	}
+	printf("%d",contadorNodo);
 }
 
 void Pila::buscar(char palabra[])
@@ -99,3 +105,122 @@ void Pila::buscar(char palabra[])
 	if(contador==0)
 		printf("\nPalabra inexistente, por favor ingrese nuevemente la palabra a buscar.\n\n");
 }
+void Pila::buscar(char palabra[],Pila *&palabrasBuscadas)
+{
+	Nodo *aux=palabras;
+	int contador=0;
+	char ingles[20];
+	strcpy(ingles,"\0");
+	while(aux!=NULL)
+	{
+		if(strcmp(palabra,aux->getEspaniol())==0)
+		{
+			strcpy(ingles,aux->getIngles());
+			palabrasBuscadas->pushNew(palabra,ingles,0);
+		}
+		aux=aux->getSiguienteDireccion();
+	}
+}
+
+void Pila::pushNew(char palabraEspaniol[],char palabrasIngles[],int i)
+{
+	Nodo *nuevo=new Nodo();
+	nuevo->setEspaniol(palabraEspaniol);
+	nuevo->setIngles(palabrasIngles);
+	if(contadorNodo==0)
+	{
+		nuevo->setSiguienteDireccion(NULL);
+	}
+	else
+	{
+		nuevo->setSiguienteDireccion(palabras);
+	}
+	palabras=nuevo;
+	contadorNodo++;	
+}
+
+
+void Pila::pushNew(char palabraEspaniol[],char palabrasIngles[])
+{
+	Nodo *nuevo=new Nodo();
+	FILE *archivoPalabras=fopen("D:\\palabrasDiccionario.txt","a+");
+	nuevo->setEspaniol(palabraEspaniol);
+	nuevo->setIngles(palabrasIngles);
+	if(contadorNodo==0)
+	{
+		nuevo->setSiguienteDireccion(NULL);
+	}
+	else
+	{
+		nuevo->setSiguienteDireccion(palabras);
+	}
+	palabras=nuevo;
+	contadorNodo++;
+	printf("\n\t   Nueva palabra ingresada.\n\n\tEspa%col\t\t\tIngles\n\n",164);
+	printf(" =>\t%s\t\t\t%s\n\n",palabras->getEspaniol(),palabras->getIngles());
+	fprintf(archivoPalabras,"\n%s;%s",palabras->getEspaniol(),palabras->getIngles());
+	fclose(archivoPalabras);	
+}
+
+void Pila::popPalabra(char palabra[])
+{
+	Nodo *aux=palabras;
+	int contador=0;
+
+	while(aux!=NULL)
+	{
+		if(strcmp(palabra,aux->getEspaniol())==0)
+		{
+			aux=NULL;
+			contadorNodo--;
+			contador++;
+		}
+		aux=aux->getSiguienteDireccion();
+	}
+	if(contador==0)
+		printf("\nPalabra inexistente, por favor ingrese nuevemente la palabra a buscar.\n\n");
+}
+
+void Pila::nombreBakcup(char nombre[],int i)
+{
+	time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+    char output[128];
+    char direcion[2000];
+    if(i==1){
+    	strcpy(direcion,"rename D:\\nombre.txt ");	
+	}else if(i==2){
+		strcpy(direcion,"rename C:\\Users\\RAMIRO BORJA F\\OneDrive - Escuela Politécnica del Ejército\\ESPE\\Estructuras De Datos\\Proyectos-Estructura-de-Datos-NRC-2480\\2do parcial\\nombre.txt ");
+	}
+    strftime(output,128,"fecha:%d-%m-%y-hora%H-%M-%S",tlocal);
+    strcpy(nombre,"Backup(");
+    strcat(nombre,output);
+	strcat(nombre,").txt");
+    printf("%s\n",nombre);
+    strcat(direcion,nombre);
+    strcpy(nombre,direcion);
+}
+
+void Pila::crearBackup(char nombre[],int i){
+	Nodo *nuevo=palabras;
+	nombreBakcup(nombre,i);
+	printf("%s\n",nombre);
+	system("pause");
+	FILE *arcBackup=NULL;
+	if(i==1){
+		arcBackup=fopen("D:\\nombre.txt","w");	
+	}else if(i==2){
+		arcBackup=fopen("C:\\Users\\RAMIRO BORJA F\\OneDrive - Escuela Politécnica del Ejército\\ESPE\\Estructuras De Datos\\Proyectos-Estructura-de-Datos-NRC-2480\\2do parcial\\nombre.txt","w");
+	}
+	
+	//INGRESO DE DATOS DE LA PILAS
+	while(nuevo!=NULL)
+	{
+		fprintf(arcBackup,"%s;%s\n",nuevo->getEspaniol(),nuevo->getIngles());
+		nuevo=nuevo->getSiguienteDireccion();
+	}
+	printf("%d",contadorNodo);
+	fclose(arcBackup);
+	system(nombre);
+}
+
--- 2do parcial/Traductor.cpp
+++ 2do parcial/Traductor.cpp
@@ -2,19 +2,21 @@
 #include "Pila.h"
 
 int main(int argc, char** argv) {
-	int opcion,opcionTraductor;
+	int opcion,opcionTraductor,opcBack;
 	bool flag=true,flagTraslate=true;
-	char palabra[25];
+	char palabra[25],palabraSpa[25],palabraEng[25];
 	char *opciones[]={"1) Traductor / Traslater.","2) Imagen.","3) Consulta PDF.","4) Codigo QR.","5) Ayuda o PULSE \"F1\".","6) Backup.","7) Base de Datos (Mongo).","8) Salir / Exit."};
 	char *opcionesTraductor[]={"1) Traducir.","2) Insertar palabra al diccionario.","3) Eliminar palabra del diccionario.","4) Volver al Menu Principal"};
+	char *opcionesBackup[]={"1) Crear Backup","2) Recuperar informacion","3) SALIR"};
 	Pila *objPalabra=new Pila();
-	
-	
+	Pila *palabrasBuscadas=new Pila();
+	char nombre[2000];
 	AltEnter();
 	ShellExecute(NULL, TEXT("open"),TEXT("Extras\\WinAppMSAgentsManagementBienvenida.exe"),NULL, NULL,SW_SHOWNORMAL);
+	objPalabra->generarPila();
 	do
 	{
-		objPalabra->generarPila();
+		flag=true;
 		opcion=menu("Menu Principal",opciones,8);
 		system("cls");
 		switch(opcion)
@@ -22,6 +24,7 @@ int main(int argc, char** argv) {
 			case 1:
 				do
 				{
+					flagTraslate=true;
 					opcionTraductor=menu("Traductor / Traslater",opcionesTraductor,4);
 					system("cls");
 					switch(opcionTraductor)
@@ -33,9 +36,33 @@ int main(int argc, char** argv) {
 								fflush(stdin);
 								gets(palabra);
 								objPalabra->buscar(palabra);
+								objPalabra->buscar(palabra,palabrasBuscadas);
 							}while(validacionCaracter(palabra));
-								
 							break;
+						case 2:
+							do
+							{
+								printf("Ingrese la palabra en espa%col que desea agregar al diccionario: ",164);
+								fflush(stdin);
+								gets(palabraSpa);
+							}while(validacionCaracter(palabraSpa));
+							do
+							{
+								printf("Ingrese la palabra en ingles que desea agregar al diccionario: ",164);
+								fflush(stdin);
+								gets(palabraEng);
+							}while(validacionCaracter(palabraEng));
+							objPalabra->pushNew(palabraSpa,palabraEng);
+							break;
+						case 3:
+							/*do
+							{
+								printf("Ingrese la palabra que desea eliminar: ");
+								fflush(stdin);
+								gets(palabra);
+								objPalabra->popPalabra(palabra); corregir el eliminar
+							}while(validacionCaracter(palabra));
+							break;*/
 						case 4:
 							flagTraslate=false;
 							break;
@@ -62,10 +89,25 @@ int main(int argc, char** argv) {
 				ShellExecute(NULL, TEXT("open"),TEXT("Extras\\Ayuda-Hotel.chm"),NULL, NULL,SW_SHOWNORMAL);		
 				break;
 			case 6:
+				opcBack=1;
+				do{
+					opcBack=menu("RESPALDO DE INFORMACION",opcionesBackup,3);
+					switch(opcBack){
+						case 1:
+							palabrasBuscadas->crearBackup(nombre,1);
+							palabrasBuscadas->crearBackup(nombre,2);
+							break;
+						case 2:
+							
+							break;
+					}
+				}while(opcBack!=3);
+				
 				
 				break;
 			case 7:
-				
+				objPalabra->imprimir();
+				system("pause");
 				break;
 			case 8:
 				printf("Gracias por utilizar el traductor.\n\n\n");
